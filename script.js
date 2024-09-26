acb11e7d82e27fbb9d8f698c3aaec9860faae834
Hellhammer12345 <168800026+Hellhammer12345@users.noreply.github.com>
2024-09-26 13:18:27 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function hello () {
+    alert ("Temur Bolkvadze") 
+} 
+
+
+       
+function getNumber (x) {
+    if (x > 50) {
+        console.log("მეტია 50ზე");
+       } else if (x <  50) {
+        console.log("ნაკლებია 50 ზე");
+       } else if (x == 50) {
+        console.log("ტოლია 50 ის");
+       } else {
+        console.log("F");
+       }
+    }
